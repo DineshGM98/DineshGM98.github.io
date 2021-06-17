5ed3efe51d6e717e216b0c504a6df5d8479831c9
DineshGM98 <93296@fhss.sjp.ac.lk>
2021-06-17 18:01:12 +0530

    Updates

---
+++ precache-manifest.bf6bd9fa74b668a99f56547a6bb633ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8733b58420128ecdc357c490e3b64c48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4634cf0dd4fd04c02ef1",
+    "url": "/static/css/main.167fb3e4.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4634cf0dd4fd04c02ef1",
+    "url": "/static/js/main.79cdd7a4.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "8f37a1bd5b885a4db3891c751f0ffb44",
+    "url": "/static/media/hashirshoaeb.8f37a1bd.png"
+  },
+  {
+    "revision": "359f77b1d062d72ac5a595de7b1e0370",
+    "url": "/static/media/resume.359f77b1.pdf"
+  }
+]);
